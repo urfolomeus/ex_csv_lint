040d31901485f91d4bc756654ad32cdeaff1cc04
Alan Gardner <urfolomeus@gmail.com>
2015-07-06 07:07:41 +0100

    Ensure that the CSV given is not blank

--- lib/ex_csv_lint.ex
+++ lib/ex_csv_lint.ex
@@ -1,2 +1,7 @@
 defmodule ExCsvLint do
+  def valid?(nil), do: {:error, "No content"}
+  def valid?(""),  do: {:error, "No content"}
+  def valid?(csv) do
+    {:ok, csv}
+  end
 end
--- test/ex_csv_lint_test.exs
+++ test/ex_csv_lint_test.exs
@@ -1,7 +1,16 @@
 defmodule ExCsvLintTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "#valid? is valid if passed valid CSV" do
+    csv = "a,b,c\n1,2,3\n4,5,6"
+    assert {:ok, csv} = ExCsvLint.valid?(csv)
+  end
+
+  test "#valid? returns 'No content' error if passed nil" do
+    assert {:error, "No content"} = ExCsvLint.valid?(nil)
+  end
+
+  test "#valid? returns 'No content' error if passed an empty string" do
+    assert {:error, "No content"} = ExCsvLint.valid?("")
   end
 end
